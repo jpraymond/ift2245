b350607aff3d4ffb1206871109423e426d61a135
System Administrator <root@MacBook-Pro-de-Jean-Philippe.local>
2015-02-03 12:58:31 -0500

    Fichiers fournis dans l'énoncé

---
+++ tp1/ptree.cpp
@@ -0,0 +1,19 @@
+/* File system */
+#include <dirent.h>
+
+/* Streams */
+#include <iostream>  /* IO stream */
+#include <fstream>   /* File stream */
+#include <sstream>   /* String stream */
+
+/* Data structures */
+#include <string>
+#include <list>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "Bonjour" << endl;
+}
---
+++ tp1/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Reste du code :)
+
