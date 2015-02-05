a283ecbbc4ef5d9c2541399164fc29be917af28f
System Administrator <root@MacBook-Pro-de-Jean-Philippe.local>
2015-02-04 20:59:01 -0500

    Ajout des processus enfants se fait maintenant par référence (avec des pointeurs)

--- tp1/processus.cpp
+++ tp1/processus.cpp
@@ -1,6 +1,3 @@
-// TODO: By reference, by value, ...?
-// TODO: Tester sur VirtualBox.
-
 #include <iostream>
 #include <string>
 #include <list>
@@ -14,30 +11,30 @@ class Processus{
 
     int id;
     string nom;
-    list<Processus> enfants;
+    list<Processus*> enfants;
 
     void imprimer_aux (int niveau_indentation);
 
     public:
-        Processus (int, string);
-        void ajouter_enfant (Processus);
+        Processus (int, const string&);
+        void ajouter_enfant (Processus&);
         void imprimer ();
 };
 
-Processus::Processus (int id, string nom) {
+Processus::Processus (int id, const string &nom) {
     this->id = id;
     this->nom = nom;
 }
 
-void Processus::ajouter_enfant (Processus enfant) {
-    enfants.push_back (enfant);
+void Processus::ajouter_enfant (Processus &enfant) {
+    enfants.push_back (&enfant);
 }
 
 void Processus::imprimer_aux (int niveau_indentation) {
     string indentation (niveau_indentation * TAILLE_INDENTATION, ' ');
     cout << indentation << id << " " << nom << endl;
-    for (list<Processus>::iterator p = enfants.begin(); p != enfants.end(); ++p) {
-        p->imprimer_aux (niveau_indentation + 1);
+    for (list<Processus*>::iterator it = enfants.begin(); it != enfants.end(); ++it) {
+        (*it)->imprimer_aux (niveau_indentation + 1);
     }
 }
 
@@ -49,15 +46,18 @@ void Processus::imprimer () {
 int main () {
     Processus p (0, "Process 0");
     
-    p.ajouter_enfant ( Processus (1, "Process 1"));
+    Processus p1 (1, "Process 1");
+    p.ajouter_enfant (p1);
     
     Processus p2 (2, "Process 2");
-    p2.ajouter_enfant ( Processus (21, "Process 21"));
-    p2.ajouter_enfant ( Processus (22, "Process 22"));
+    p.ajouter_enfant (p2); 
+    Processus p21 (21, "Process 21");
+    p2.ajouter_enfant (p21);
+    Processus p22 (22, "Process 22");
+    p2.ajouter_enfant (p22);
 
-    p.ajouter_enfant(p2);
-
-    p.ajouter_enfant( Processus (3, "Process 3"));
+    Processus p3 (3, "Process 3");
+    p.ajouter_enfant (p3);
     
     p.imprimer();
     
