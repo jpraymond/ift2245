9325027ed9702fadc215953832c5ad465a9f3b51
jpraymond <raymonjp@iro.umontreal.ca>
2015-02-07 16:44:05 -0500

    processus.h

---
+++ tp1/processus.h
@@ -0,0 +1,22 @@
+#include <string>
+#include <list>
+
+
+using namespace std;
+
+
+class Processus {
+    private:
+        static const int TAILLE_INDENTATION = 2;
+
+        int id_;
+        string nom_;
+        list<Processus*> enfants;
+
+        void imprimer_aux (int);
+
+    public:
+        Processus (int, const string&);
+        void ajouter_enfant (Processus&);
+        void imprimer();
+};
