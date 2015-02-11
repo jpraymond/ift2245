ff5903aeb5358d433219a4ee83a21465ca86c2d5
larseeri <erlars2002@yahoo.com>
2015-02-11 01:11:37 -0500

    ajout de commentaires

--- tp1/processus.h
+++ tp1/processus.h
@@ -1,16 +1,25 @@
+// IFT2245
+// Devoir 1
+// 12 fevrier 2015
+// Eric Larsen
+// Jean-Philippe Raymond
+
 #include <string>
 #include <list>
-
 
 using namespace std;
 
 
+// Entete de la classe Processus
 class Processus {
     private:
         static const int TAILLE_INDENTATION = 2;
 
+	//numero du processus
         int id;
+	//nom du processus
         string nom;
+	// liste des enfants du processus
         list<Processus*> enfants;
 
         void imprimer_aux (int);
