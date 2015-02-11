a50261c2645218bcfd803139fac06998c971239b
larseeri <erlars2002@yahoo.com>
2015-02-11 01:28:09 -0500

    ajout de commentaires

--- tp1/processus.cpp
+++ tp1/processus.cpp
@@ -1,20 +1,28 @@
+// IFT2245
+// Devoir 1
+// 12 fevrier 2015
+// Eric Larsen
+// Jean-Philippe Raymond
+
 #include "processus.h"
-
 #include <iostream>
-
 
 using namespace std;
 
+// Corps de la classe Processus
 
+// constructeur
 Processus::Processus (int id, const string &nom) {
     this->id = id;
     this->nom = nom;
 }
 
+// ajout d'un enfant a la liste des enfants du processus
 void Processus::ajouter_enfant (Processus &enfant) {
     enfants.push_back (&enfant);
 }
 
+// impression formatee de l'arbre du processus
 void Processus::imprimer_aux (int niveau_indentation) {
     string indentation (niveau_indentation * TAILLE_INDENTATION, ' ');
     cout << indentation << id << " " << nom << endl;
@@ -25,6 +33,7 @@ void Processus::imprimer_aux (int niveau_indentation) {
     }
 }
 
+// impression lorsque le processus est au sommet de l'arborescence
 void Processus::imprimer () {
     imprimer_aux (0);
 }
