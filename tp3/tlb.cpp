29443ef1e658d4e62021674366ef518f6f744369
larseeri <erlars2002@yahoo.com>
2015-04-08 23:47:36 -0400

    implementation tlb

--- tp3/tlb.cpp
+++ tp3/tlb.cpp
@@ -1,31 +1,102 @@
 #include "tlb.h"
 
 TLB_entry::TLB_entry(int page, int frame) :
-    pageNumber(page), frameNumber(frame){
+  pageNumber(page), frameNumber(frame){
 }
 
-// Looks for the pageNumber in the table, if it finds it it return
-// the coresponding frame if not it returns -1;
+// recherche lineaire de la page 'pageNumber' dans la table TLB, si succes retourne le numero du cadre memoire correspondant,
+//  sinon retourne -1
+// TODO: preparer une seconde version avec tableau TLB inchange, utilisant toutefois dictionnaire auxiliaire pour la recherche
 int TLB::findPage(int pageNumber){
-    /// --------TP3__TO_DO---------
-    ///
-    ///
-    return 0;
+  int found = -1;
+  for (int i = 0; i < TLB_NUM_ENTRIES; i ++)
+    {
+      if (TLBTable[i].pageNumber == pageNumber)
+	{
+	  found = TLBTable[i].frameNumber;
+	  // mise a jour de l'estampille de la page trouvee pour permettre l'usage de addEntryLRU()
+	  sequNumbers[i] = currentSequNumber;
+	  currentSequNumber++;
+	  return found;
+	}
+    }
+  return found;
 }
 
-//Adds an entry-pair (pageNumber,frameNumber) to the TLB
-//Here you have to implement a replacement technique for
-//when the TLB is full. DONT FORGET to implement two different
-//function for this, even if you only call one of them
-void TLB::addEntry(int pageNumber, int frameNumber){
-    /// --------TP3__TO_DO---------
-    ///
-    ///
+// Ajoute une paire (pageNumber,frameNumber) au tableau TLB
+// en appliquant la methode PEPS (FIFO) lorsque le tableau TLB est plein
+void TLB::addEntryFIFO(int pageNumber, int frameNumber){   
+  // le tableau TLB n'est pas encore plein
+  if (nextEntryAvailable >= 0 && nextEntryAvailable < TLB_NUM_ENTRIES)
+    {
+      // insertion dans le tableau TLB
+      TLBTable[nextEntryAvailable].pageNumber = pageNumber;
+      TLBTable[nextEntryAvailable].frameNumber = frameNumber;
+      // mise a jour de la file PEPS
+      fifoQueue.push_back(nextEntryAvailable);
+      // mise a jour du numero du prochain emplacement libre
+      nextEntryAvailable++;
+      if (nextEntryAvailable == TLB_NUM_ENTRIES)
+	{
+	  nextEntryAvailable = -1;
+	}
+    }
+  // le tableau TLB est plein
+  else
+    {
+      // substitution dans le tableau TLB d'apres PEPS
+      int select = fifoQueue.front();
+      fifoQueue.pop_front();
+      TLBTable[select].pageNumber = pageNumber;
+      TLBTable[select].frameNumber = frameNumber;
+      // mise a jour de la file PEPS
+      fifoQueue.push_back(select);
+    }
 }
 
+// Ajoute une paire (pageNumber,frameNumber) au tableau TLB avec remplacement
+// suivant la methode MRU (LRU) lorsque le tableau TLB est plein
+void TLB::addEntryLRU(int pageNumber, int frameNumber){
+  // le tableau TLP n'est pas encore plein
+  if (nextEntryAvailable >= 0 && nextEntryAvailable < TLB_NUM_ENTRIES)
+    {
+      // insertion dans le tableau TLP
+      TLBTable[nextEntryAvailable].pageNumber = pageNumber;
+      TLBTable[nextEntryAvailable].frameNumber = frameNumber;
+      // mise a jour de l'estampille de la page nouvellement inseree
+      sequNumbers[nextEntryAvailable] = currentSequNumber;
+      currentSequNumber++;
+      // mise a jour du numero du prochain emplacement libre
+      nextEntryAvailable++;
+      if (nextEntryAvailable == TLB_NUM_ENTRIES)
+	{
+	  nextEntryAvailable = -1;
+	}   
+    }
+  // le tableau TLP est plein
+  else
+    {
+      int min = currentSequNumber;
+      int select = -1;
+      // recherche lineaire de la page la moins recemment consultee
+      for (int i = 0; i < TLB_NUM_ENTRIES; i ++)
+	{
+	  if (sequNumbers[i] <= min)
+	    {
+	      min = sequNumbers[i];
+	      select = i;
+	    }
+	}
+      // substitution dans le tableau TLP
+      TLBTable[select].pageNumber = pageNumber;
+      TLBTable[select].frameNumber = frameNumber;
+      // mise a jour de l'estampille de la page nouvellement inseree
+      sequNumbers[select] = currentSequNumber;
+      currentSequNumber++;
+    }
+}
 
-
-TLB::TLB() : nextEntryAvailable(0){
+TLB::TLB() : nextEntryAvailable(0), currentSequNumber(0){
 }
 
 TLB::~TLB(){
@@ -37,8 +108,8 @@ TLB::~TLB(){
 // be generated for evaluation purposes.
 void TLB::printTLB()
 {
-     ofstream tlbFile("TLB.txt",ios::out );\
-     for (int i=0; i<TLB_NUM_ENTRIES; ++i)
-         tlbFile << TLBTable[i].pageNumber << " " << TLBTable[i].frameNumber << endl;
-     tlbFile.close();
+  ofstream tlbFile("TLB.txt",ios::out );\
+  for (int i=0; i<TLB_NUM_ENTRIES; ++i)
+    tlbFile << TLBTable[i].pageNumber << " " << TLBTable[i].frameNumber << endl;
+  tlbFile.close();
 }
--- tp3/tlb.h
+++ tp3/tlb.h
@@ -4,39 +4,48 @@
 #include "common.h"
 
 class TLB_entry{
-public:
-    int pageNumber;
-    int frameNumber;
+ public:
+  int pageNumber;
+  int frameNumber;
 
-    TLB_entry(int page = -1, int frame = -1);
+  TLB_entry(int page = -1, int frame = -1);
 };
 
 class TLB
 {
-public:
-    TLB_entry TLBTable[TLB_NUM_ENTRIES];
+ private: 
+  // permet l'implementation du remplacement suivant PEPS (FIFO)
+  list<int> fifoQueue;
 
-    TLB();
-    ~TLB();
-    // Looks for the pageNumber in the table, if it finds it it return
-    // the coresponding frame if not it returns -1;
-    int findPage(int pageNumber);
+  // permet l'implementation du remplacement suivant MRU (LRU)
+  int currentSequNumber;
+  int sequNumbers[TLB_NUM_ENTRIES];
 
-    //Adds an entry-pair (pageNumber,frameNumber) to the TLB
-    //Here you have to implement a replacement technique for
-    //when the TLB is full. DONT FORGET to implement two different
-    //function for this, even if you only call one of them
-    void addEntry(int pageNumber, int frameNumber);
+ public:
+  TLB_entry TLBTable[TLB_NUM_ENTRIES];
 
+  TLB();
+  ~TLB();
+  // Looks for the pageNumber in the table, if it finds it it return
+  // the coresponding frame if not it returns -1;
+  int findPage(int pageNumber);
+   
+  // Ajoute une paire (pageNumber,frameNumber) au tableau TLB avec remplacement
+  // suivant la methode PEPS (FIFO) lorsque le tableau TLB est plein
+  void addEntryFIFO(int pageNumber, int frameNumber);
 
-    int nextEntryAvailable;
+  // Ajoute une paire (pageNumber,frameNumber) au tableau TLB avec remplacement
+  // suivant la methode MRU (LRU) lorsque le tableau TLB est plein
+  void addEntryLRU(int pageNumber, int frameNumber);
 
-    //----------DO NOT CHANGE/ERASE THIS------------------
-    // this function is neccesary to output the physical
-    // memory state at the end of execution. It needs to
-    // be generated for evaluation purposes.
-    void printTLB();
-    //----------------------------------------------------
+  int nextEntryAvailable;
+
+  //----------DO NOT CHANGE/ERASE THIS------------------
+  // this function is neccesary to output the physical
+  // memory state at the end of execution. It needs to
+  // be generated for evaluation purposes.
+  void printTLB();
+  //----------------------------------------------------
 };
 
 #endif // TLB_H
