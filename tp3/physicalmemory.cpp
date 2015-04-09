618fb223e566f1f214ba5780b2ac1ddbbfd48660
larseeri <erlars2002@yahoo.com>
2015-04-08 23:47:13 -0400

    implementation physical memory

--- tp3/physicalmemory.cpp
+++ tp3/physicalmemory.cpp
@@ -1,52 +1,70 @@
 #include "physicalmemory.h"
 
 PhysicalMemory::PhysicalMemory(){
-    //Open the BACKING_STORE file to being able to read from it afterwards
-    backingStoreFile.open("../VirtualMemoryManager/BACKING_STORE.txt",ios::in );
+  //Open the BACKING_STORE file to being able to read from it afterwards
+  backingStoreFile.open("../VirtualMemoryManager/BACKING_STORE.txt",ios::in );
 
-    // fills the PhysicalMemory with spaces ' '
-    for (int i=0; i<PHYSICAL_MEMORY_SIZE; ++i)
-        physicalMemoryData[i] = ' ';
+  // fills the PhysicalMemory with spaces ' '
+  // initialise la pile des cadres disponibles
+  for (int i=0; i<PHYSICAL_MEMORY_SIZE; ++i)
+    {
+      physicalMemoryData[i] = ' ';
+      freeFrames.push_front(i);
+    }
 }
 
 PhysicalMemory::~PhysicalMemory(){
-    //Close the BACKING_STORE file
-    backingStoreFile.close();
+  //Close the BACKING_STORE file
+  backingStoreFile.close();
 }
 
-//Looks for a free frame to store the page read from the BACKING_STORE file
+// retourne un numero de cadre libre et met a jour la piled des cadres disponibles
 int PhysicalMemory::findFreeFrame(){
-    /// --------TP3__TO_DO---------
-    ///
-    ///
-    return 0;
+  int frameNumber = freeFrames.front();
+  freeFrames.pop_front();
+  return frameNumber;
 }
 
-int PhysicalMemory::demandPageFromBackingStore(unsigned int pageNumber){
-    /// --------TP3__TO_DO---------
-    ///
-    ///
-    return 0;
+// TODO: configurer correctement la valeur retournee
+// cherche la page indiquee dans la reserve et la copie dans le cadre indique
+int PhysicalMemory::demandPageFromBackingStore(unsigned int fromPageNumber, unsigned int toFrameNumber)
+{  
+  // nombre d'entrees apparaissant dans les pages precedentes de la reserve
+  unsigned int beforePage = fromPageNumber * PAGE_FRAME_SIZE;
+  char token;
+  // parcours des entrees situees dans les pages precedentes
+  for (int i = 0; i < beforePage; i++)
+    {
+      backingStoreFile >> token;
+    }
+  // nombre d'entrees apparaissant dans les cadres precedents de la memoire
+  int beforeFrame = toFrameNumber * PAGE_FRAME_SIZE;
+  // copie la page demandee depuis la reserve vers la memoire
+  for (int i = 0; i < PAGE_FRAME_SIZE; i++)
+    {
+      backingStoreFile >> token;
+      physicalMemoryData[beforeFrame + i] = token;
+    }
+  // reinitialisation de la reserve
+  backingStoreFile.clear();
+  backingStoreFile.seekg(0, ios::beg);
+  return 0;
 }
 
+// retourne l'entree se trouvant au cadre et decalage indiques
 char PhysicalMemory::getValueFromFrameAndOffset(unsigned int frameNumber, unsigned int offset){
-    /// --------TP3__TO_DO---------
-    ///
-    ///
-    return 0;
+  unsigned int position = frameNumber * PAGE_FRAME_SIZE + offset;
+  return physicalMemoryData[position];
 }
-
-
-
 
 //----------DO NOT CHANGE/ERASE THIS------------------
 // this function is neccesary to output the physical
 // memory state at the end of execution. It needs to
 // be generated for evaluation purposes.
 void PhysicalMemory::printPhysicalMemory(){
-    ofstream physicalMemoryFile("PhysicalMemory.txt",ios::out );
-    for (int i=0; i<PHYSICAL_MEMORY_SIZE; ++i)
-        physicalMemoryFile << physicalMemoryData[i];
-    physicalMemoryFile.close();
+  ofstream physicalMemoryFile("PhysicalMemory.txt",ios::out );
+  for (int i=0; i<PHYSICAL_MEMORY_SIZE; ++i)
+    physicalMemoryFile << physicalMemoryData[i];
+  physicalMemoryFile.close();
 }
 
--- tp3/physicalmemory.h
+++ tp3/physicalmemory.h
@@ -2,31 +2,36 @@
 #define PHYSICALMEMORY_H
 
 #include "common.h"
+#include <list>
 
 class PhysicalMemory
 {
-public:
-    char physicalMemoryData[PHYSICAL_MEMORY_SIZE];
-    ifstream backingStoreFile;
 
-    PhysicalMemory();
-    ~PhysicalMemory();
+ private:
+  list<int> freeFrames;
 
-    //Looks for a free frame to store the page read from the BACKING_STORE file
-    int findFreeFrame();
+ public:
+  char physicalMemoryData[PHYSICAL_MEMORY_SIZE];
+  ifstream backingStoreFile;
 
-    //Reads a certain page (256 bytes) from the BACKING_STORE, starting at page 'pageNumber'
-    int demandPageFromBackingStore(unsigned int pageNumber);
+  PhysicalMemory();
+  ~PhysicalMemory();
 
-    //Given the frameNumber and the offset get the char value at that physical address
-    char getValueFromFrameAndOffset(unsigned int frameNumber, unsigned int offset);
+  //Looks for a free frame to store the page read from the BACKING_STORE file
+  int findFreeFrame();
 
-    //----------DO NOT CHANGE/ERASE THIS------------------
-    // this function is neccesary to output the physical
-    // memory state at the end of execution. It needs to
-    // be generated for evaluation purposes.
-    void printPhysicalMemory();
-    //----------------------------------------------------
+  //Reads a certain page (256 bytes) from the BACKING_STORE, starting at page 'pageNumber'
+  int demandPageFromBackingStore(unsigned int fromPageNumber, unsigned int toFrameNumber);
+
+  //Given the frameNumber and the offset get the char value at that physical address
+  char getValueFromFrameAndOffset(unsigned int frameNumber, unsigned int offset);
+
+  //----------DO NOT CHANGE/ERASE THIS------------------
+  // this function is neccesary to output the physical
+  // memory state at the end of execution. It needs to
+  // be generated for evaluation purposes.
+  void printPhysicalMemory();
+  //----------------------------------------------------
 };
 
 #endif // PHYSICALMEMORY_H
